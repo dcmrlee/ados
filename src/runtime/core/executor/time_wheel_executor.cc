a8b64230595a5171459a7aed1fffdcffea7ec7e4
lidachao <lidachao@xiaomi.com>
2025-01-16 14:57:12 +0800

    add time wheel executor

--- src/runtime/core/executor/time_wheel_executor.cc
+++ src/runtime/core/executor/time_wheel_executor.cc
@@ -1,6 +1,7 @@
 // Copyright (C) 2024. All rights reserved.
 
 #include "runtime/core/executor/time_wheel_executor.h"
+#include "utils/common/thread_tool.h"
 
 namespace YAML {
 template <>
@@ -45,12 +46,51 @@ void TimeWheelExecutor::Initialize(std::string_view name, YAML::Node options_nod
   if (options_node && !options_node.IsNull()) {
     options_ = options_node.as<Options>();
   }
+
+  dt_count_ = static_cast<uint64_t>(
+      std::chrono::duration_cast<std::chrono::nanoseconds>(options_.dt).count());
+
+  uint64_t cur_scale = 1;
+  for (size_t ii = 0; ii < options_.wheel_size.size(); ++ii) {
+    uint64_t start_pos = (ii == 0) ? 0 : 1;
+    timing_wheel_vec_.emplace_back(TimingWheelTool{
+        .current_pos = start_pos,
+        .scale = (cur_scale *= options_.wheel_size[ii]),
+        .wheel = std::vector<TaskList>(options_.wheel_size[ii]),
+        .borrow_func = [ii, this]() {
+          TaskList task_list;
+          if (ii < options_.wheel_size.size() - 1) {
+            task_list = timing_wheel_vec_[ii + 1].Tick();
+          } else {
+            auto iter = timing_task_map_.find(timing_task_map_pos_);
+            ++timing_task_map_pos_;
+            if (iter != timing_task_map_.end()) {
+              task_list = std::move(iter->second);
+              timing_task_map_.erase(iter);
+            }
+
+            while (!task_list.empty()) {
+              auto iter = task_list.begin();
+              auto& cur_list =
+                  timing_wheel_vec_[ii].wheel[iter->tick_count % timing_wheel_vec_[ii].scale];
+              cur_list.splice(cur_list.end(), task_list, iter);
+            }
+          }
+        }});
+  }
+
+  timing_task_map_pos_ = 1;
+
   NXPILOT_INFO("TimeWheelExecutor init completed");
 }
 
 void TimeWheelExecutor::Start() {
   NXPILOT_CHECK_ERROR(std::atomic_exchange(&state_, State::kStart) == State::kInit,
                       "TimeWheelExecutor can only run when state is 'Init'.");
+
+  timer_thread_ptr_ = std::make_unique<std::thread>(std::bind(&TimeWheelExecutor::TimerLoop, this));
+
+  start_flag_.wait(false);
   NXPILOT_INFO("TimeWheelExecutor start completed");
 }
 
@@ -58,6 +98,14 @@ void TimeWheelExecutor::Shutdown() {
   if (std::atomic_exchange(&state_, State::kShutdown) == State::kShutdown) {
     return;
   }
+
+  if (timer_thread_ptr_ && timer_thread_ptr_->joinable()) {
+    timer_thread_ptr_->join();
+  }
+
+  timer_thread_ptr_.reset();
+  timing_task_map_.clear();
+  timing_wheel_vec_.clear();
 
   NXPILOT_INFO("TimeWheelExecutor shutdown");
 }
@@ -67,12 +115,104 @@ void TimeWheelExecutor::Execute(Task&& task) noexcept {
 }
 
 std::chrono::system_clock::time_point TimeWheelExecutor::Now() const noexcept {
-  NXPILOT_ERROR("TimeWheelExecutor does not support timer schedule");
-  return std::chrono::system_clock::time_point();
+  std::shared_lock<std::shared_mutex> lck(tick_mutex_);
+
+  return nxpilot::utils::common::GetTimePointFromTimestampNs(current_tick_count_ * dt_count_ +
+                                                             start_time_point_);
 }
 
 void TimeWheelExecutor::ExecuteAt(std::chrono::system_clock::time_point tp, Task&& task) noexcept {
-  NXPILOT_ERROR("TimeWheelExecutor does not support timer schedule");
+  try {
+    uint64_t virtual_tp = nxpilot::utils::common::GetTimestampNs(tp) - start_time_point_;
+
+    std::unique_lock<std::shared_mutex> lck(tick_mutex_);
+
+    if (virtual_tp < current_tick_count_ * dt_count_) {
+      lck.unlock();
+      Execute(std::move(task));
+      return;
+    }
+
+    uint64_t tmp_current_tick_count = current_tick_count_;
+    uint64_t diff_tick_count = virtual_tp / dt_count_ - current_tick_count_;
+
+    const size_t len = options_.wheel_size.size();
+    for (size_t ii = 0; ii < len; ++ii) {
+      if (diff_tick_count < options_.wheel_size[ii]) {
+        auto pos = (diff_tick_count + tmp_current_tick_count) % options_.wheel_size[ii];
+        timing_wheel_vec_[ii].wheel[pos].emplace_back(
+            TaskWithTimestamp{virtual_tp / dt_count_, std::move(task)});
+        return;
+      }
+
+      diff_tick_count /= options_.wheel_size[ii];
+      tmp_current_tick_count /= options_.wheel_size[ii];
+    }
+    timing_task_map_[diff_tick_count + tmp_current_tick_count].emplace_back(
+        TaskWithTimestamp{virtual_tp / dt_count_, std::move(task)});
+  } catch (const std::exception& e) {
+    NXPILOT_ERROR("{}", e.what());
+  }
+}
+
+void TimeWheelExecutor::TimerLoop() {
+  thread_id_ = std::this_thread::get_id();
+
+  try {
+    nxpilot::utils::common::SetNameForCurrentThread(name_);
+    nxpilot::utils::common::BindCpuForCurrentThread(options_.thread_bind_cpu);
+    nxpilot::utils::common::SetCpuSchedForCurrentThread(options_.thread_sched_policy);
+  } catch (const std::exception& e) {
+    NXPILOT_ERROR("Set thread policy for TimeWheelExecutor get exception, {}", e.what());
+  }
+
+  auto last_loop_time_point = std::chrono::system_clock::now();
+
+  start_time_point_ = nxpilot::utils::common::GetTimestampNs(last_loop_time_point);
+  start_flag_.store(true);
+  start_flag_.notify_all();
+
+  while (state_.load() != State::kShutdown) {
+    try {
+      auto real_dt = options_.dt;
+      do {
+        static constexpr auto kMaxSleepDt = std::chrono::seconds(1);
+        auto sleep_time = (real_dt > kMaxSleepDt) ? kMaxSleepDt : real_dt;
+        real_dt -= sleep_time;
+
+        if (real_dt.count() && options_.dt < kMaxSleepDt && real_dt <= options_.dt) {
+          sleep_time += real_dt;
+          real_dt -= real_dt;
+        }
+
+        std::this_thread::sleep_until(
+            last_loop_time_point +=
+            std::chrono::duration_cast<std::chrono::system_clock::time_point::duration>(
+                sleep_time));
+
+      } while (state_.load() != State::kShutdown && real_dt.count());
+
+      tick_mutex_.lock();
+      TaskList task_list = timing_wheel_vec_[0].Tick();
+      if (!task_list.empty()) {
+        tick_mutex_.unlock();
+        for (auto& iter : task_list) {
+          try {
+            iter.task();
+          } catch (const std::exception& e) {
+            NXPILOT_FATAL("TimeWheelExecutor run task get exception, {}", e.what());
+          }
+        }
+        tick_mutex_.lock();
+      }
+
+      ++current_tick_count_;
+      tick_mutex_.unlock();
+
+    } catch (const std::exception& e) {
+      NXPILOT_FATAL("TimeWheelExecutor run loop get exception, {}", e.what());
+    }
+  }
 }
 
 }  // namespace nxpilot::runtime::core::executor
--- src/runtime/core/executor/time_wheel_executor.h
+++ src/runtime/core/executor/time_wheel_executor.h
@@ -4,7 +4,10 @@
 
 #include <atomic>
 #include <functional>
+#include <shared_mutex>
 #include <thread>
+#include <memory>
+#include <mutex>
 
 #include "runtime/core/executor/executor_base.h"
 #include "utils/common/log_tool.h"
@@ -57,6 +60,35 @@ class TimeWheelExecutor : public ExecutorBase {
   size_t CurrentTaskNum() noexcept override { return 1; }
 
  private:
+  void TimerLoop();
+  struct TaskWithTimestamp {
+    uint64_t tick_count;  // 距离start_time的时间tick
+    Task task;
+  };
+
+  using TaskList = std::list<TaskWithTimestamp>;
+
+  struct TimingWheelTool {
+    uint64_t current_pos;
+    uint64_t scale;
+    std::vector<TaskList> wheel;
+    std::function<void()> borrow_func;
+
+    TaskList Tick() {
+      TaskList task_list;
+      task_list.swap(wheel[current_pos]);
+
+      ++current_pos;
+      if (current_pos == wheel.size()) [[unlikely]] {
+        current_pos = 0;
+        borrow_func();
+      }
+
+      return task_list;
+    }
+  };
+
+ private:
   std::shared_ptr<nxpilot::utils::common::Logger> logger_ptr_;
   Options options_;
   std::atomic<State> state_ = State::kPreInit;
@@ -64,6 +96,18 @@ class TimeWheelExecutor : public ExecutorBase {
   std::string name_;
   std::thread::id thread_id_;
   std::string_view type_ = "time_wheel";
+
+  uint64_t dt_count_;
+  std::vector<TimingWheelTool> timing_wheel_vec_;  // 多级时间轮
+  uint64_t timing_task_map_pos_ = 0;
+  std::map<uint64_t, TaskList> timing_task_map_;
+
+  std::unique_ptr<std::thread> timer_thread_ptr_;
+  std::atomic_bool start_flag_ = false;
+  uint64_t start_time_point_ = 0;
+
+  mutable std::shared_mutex tick_mutex_;
+  uint64_t current_tick_count_ = 0;
 };
 
 }  // namespace nxpilot::runtime::core::executor
